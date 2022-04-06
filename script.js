7ca6fc9b15924cf7caeb95d7aaebb60dc07c4f8b
Scott Fillmer <scottfillmer@me.com>
2022-04-06 08:57:32 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10);
+}
+
+console.log(generateTarget());
+
+const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
+  const humanDiff = Math.abs(targetGuess - humanGuess);
+  const computerDiff = Math.abs(targetGuess - computerGuess);
+   /* if else version 
+   if(humanDiff <= computerDiff) {
+     return true;
+   } else false;
+   */
+   return humanDiff <= computerDiff;
+}
+
+const updateScore = winner => {
+  if (winner === 'human') {
+    humanScore++;
+  } else if (winner === 'computer') {
+    computerScore++;
+  }
+}
+
+const advanceRound = () =>
+currentRoundNumber++;
